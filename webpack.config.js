8bb4cd27207ea3c70aa893d92b180fa91c8e2c12
Smaddermand <50303469+Smaddermand@users.noreply.github.com>
2024-02-17 21:15:12 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/script.js', // Adjust if your entry file is located elsewhere
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Adjust if your HTML file is located elsewhere
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
